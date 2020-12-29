aa0f6b19aeb4157bb2bed696a8778fa790a568b1
fov42550564 <42550564@qq.com>
2020-12-29 23:53:31 +0800

    修正历史

--- project/js/history.js
+++ project/js/history.js
@@ -1,25 +1,11 @@
 (function() {
+    let vm;
     let history = []; // 历史记录
     let historyIndex = 0; // 当前历史记录的指针
     let lastAction = {}; // 上一次操作的对象
 
-    function optimizeHistory() {
-        const length = history.length;
-        if (length > 0) {
-            const dialog = layer.confirm('优化历史记录了之后不能再恢复，是否继续?', {
-                btn: ['继续','放弃']
-            }, function(){
-                let newHistory = [];
-                for (let i = 0; i < length - 10; i += 10) {
-                    newHistory.push(history[i]);
-                }
-                history = newHistory.concat(history.slice(history.length-10));
-                historyIndex = history.length - 1;
-                showHistory();
-                layer.close(dialog);
-                utils.toast('优化成功');
-            });
-        }
+    function initHistory(_vm) {
+        vm = _vm;
     }
     function showHistory() {
         document.getElementById('historyContent').innerHTML = history.map((o, k)=>(
@@ -27,16 +13,10 @@
         )).join('');
     }
     function pushHistory(name) {
-        const action = editor.getAction();
-        if (!(lastAction.target === action.target && lastAction.name === name)) {
-            lastAction.target = action.target;
-            lastAction.name = name;
-
-            historyIndex++;
-            history.length = historyIndex;
-            history.push({ name, json: vm.geojsonInput() });
-            showHistory();
-        }
+        historyIndex++;
+        history.length = historyIndex;
+        history.push({ name, json: vm.geojsonInput() });
+        showHistory();
     }
     function setTopHistory(index) {
         historyIndex = index;
@@ -53,5 +33,9 @@
             setTopHistory(historyIndex + 1);
         }
     }
-    window.splitPolygon = split;
+    window.initHistory = initHistory;
+    window.pushHistory = pushHistory;
+    window.popHistory = popHistory;
+    window.recoverHistory = recoverHistory;
+    window.setTopHistory = setTopHistory;
 })();
